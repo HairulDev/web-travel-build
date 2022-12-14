ec071ffab1498a1c3cf07e0c90784d5da9bc0f3e
Hairul <98758226+HairulDev@users.noreply.github.com>
2022-12-14 14:58:10 +0700

    first build

---
+++ precache-manifest.06c916e9c6954d84dd2828b7700095ac.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90176c94719a611d60c2f06ebbc6d7c2",
+    "url": "https://sambasvacation.netlify.app/index.html"
+  },
+  {
+    "revision": "b28124e2b4015ee76661",
+    "url": "https://sambasvacation.netlify.app/static/css/2.ae4320a7.chunk.css"
+  },
+  {
+    "revision": "80357b81302d1b63ab9d",
+    "url": "https://sambasvacation.netlify.app/static/css/main.968fa100.chunk.css"
+  },
+  {
+    "revision": "b28124e2b4015ee76661",
+    "url": "https://sambasvacation.netlify.app/static/js/2.63cb09d2.chunk.js"
+  },
+  {
+    "revision": "00191d594a69ca831f39c550dbd01cba",
+    "url": "https://sambasvacation.netlify.app/static/js/2.63cb09d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80357b81302d1b63ab9d",
+    "url": "https://sambasvacation.netlify.app/static/js/main.6ee7c000.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "https://sambasvacation.netlify.app/static/js/main.6ee7c000.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d03a5fa299392ee1891",
+    "url": "https://sambasvacation.netlify.app/static/js/runtime-main.65c5cf39.js"
+  },
+  {
+    "revision": "3366f1365b8130f7b2eff3d2b3bba490",
+    "url": "https://sambasvacation.netlify.app/static/media/completed.3366f136.jpg"
+  },
+  {
+    "revision": "67e25a39beb992b5476840cc2ce5aa21",
+    "url": "https://sambasvacation.netlify.app/static/media/icon-calendar.67e25a39.svg"
+  },
+  {
+    "revision": "60e23ce86bd69731659ed18adf81db74",
+    "url": "https://sambasvacation.netlify.app/static/media/img-hero.60e23ce8.jpg"
+  },
+  {
+    "revision": "11a62c712d17eaee33a02927afd01988",
+    "url": "https://sambasvacation.netlify.app/static/media/star.11a62c71.svg"
+  }
+]);
